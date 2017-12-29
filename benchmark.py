3ac4f207572560b309e8898d4f85eb92165a38cb
tappyy <apottage@gmail.com>
2017-12-29 01:49:56 +0000

    improved benchmarking, added lowercase estimations

--- benchmark.py
+++ benchmark.py
@@ -11,32 +11,22 @@ import wmi
 
 CONFIG_FILE = 'config.ini'
 RANDOM_PASSWORD_LENGTH = 10
-MAX_GUESS_LIMIT = 100000
+MAX_GUESS_LIMIT = 5000000
 computer = wmi.WMI()
 cpu_info = computer.Win32_Processor()[0]
 
 def get_guess_rate():
-    try:
-        # read file and return guess rate
-        config = configparser.ConfigParser()
-        config.read(CONFIG_FILE)
-
-        guess_rate = config.get('cpu_settings', 'guess_rate')
-        return guess_rate
-    except:
-        # file doesn't exist, create one and perform benchmark
+    if not(os.path.isfile(CONFIG_FILE)):
         create_config_file()
-        timed_brute_force()
 
-        config = configparser.ConfigParser()
-        config.read(CONFIG_FILE)
-        guess_rate = config.get('cpu_settings', 'guess_rate')
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
 
-        return guess_rate
+    guess_rate = config.get('cpu_settings', 'guess_rate')
+    return guess_rate
 
 def timed_brute_force():
-    # perform brute force attack to get average guesses benchmark
-    # gather specs about the CPU
+    # add progress bar
     time_start = time.time()
     guesses = 0
     random_password = create_random_password()
@@ -46,19 +36,17 @@ def timed_brute_force():
 
     for guess in itertools.product(string.ascii_letters + string.punctuation, repeat=RANDOM_PASSWORD_LENGTH):
         guesses += 1
-        print('>>> Attempts: {}'.format(guesses), end='\r', flush=True)
+        # print('>>> Attempts: {}'.format(guesses), end='\r', flush=True)
         if(guesses == MAX_GUESS_LIMIT):
-            # update config.ini with guess rate
             time_finish = time.time()
             time_taken = time_finish - time_start
             guess_rate = guesses / time_taken
-            print('Guess target of {} reached. Guess rate per second: {:.0f} - Time taken: {:.2f} seconds'.format(MAX_GUESS_LIMIT, guess_rate, time_taken))
             update_config_file('{:.0f}'.format(guess_rate))
+            print('Guess target of {} reached. Guess rate per second: {:.0f} - Time taken: {:.2f} seconds'.format(MAX_GUESS_LIMIT, guess_rate, time_taken))
             break
 
 def create_random_password():
     random_password = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(RANDOM_PASSWORD_LENGTH)])
-
     return random_password
 
 def create_config_file():
@@ -69,12 +57,14 @@ def create_config_file():
     config.add_section('cpu_settings')
     config.set('cpu_settings', 'cpu_model', str(cpu_info.Name))
     config.set('cpu_settings', 'cpu_logical', str(os.cpu_count()))
-    config.set('cpu_settings', 'guess_rate', str(11000)) # default value
+    config.set('cpu_settings', 'guess_rate', str(15000)) # default value
 
     with open(CONFIG_FILE, "w") as f:
         config.write(f)
 
     print('.ini file created')
+
+    timed_brute_force()
 
 def update_config_file(guess_rate):
     config = configparser.ConfigParser()
--- qwertyuiop.py
+++ qwertyuiop.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python
-from benchmark import get_guess_rate
+from benchmark import *
 import os.path
 import argparse
 
+# add output to csv option
 parser = argparse.ArgumentParser()
 parser.add_argument("pwd", type=str,
-                    help='the password to test')
+                    help="The password to test")
+parser.add_argument("-b", "--benchmark",
+                    help="Perform a benchmark to get guess rate", action="store_true")
 
 args = parser.parse_args()
 password = args.pwd
-guess_rate = get_guess_rate()
+
+# create config file if it doesn't exist
+if not(os.path.isfile(CONFIG_FILE)):
+    create_config_file()
+else:
+    if(args.benchmark): # stops performing benchmark twice
+        timed_brute_force()
+
+guess_rate = int(get_guess_rate())
+search_space = 26**len(password)
 print('guess rate is {} guesses per second'.format(guess_rate))
 
-# calculate time to brute force password given guess rate
+crack_time_seconds = search_space / guess_rate
+crack_time_minutes = crack_time_seconds / 60
+crack_time_hours = crack_time_minutes / 60
+crack_time_days = crack_time_hours / 24
+
+print('Estimated time to crack password: {:.2f} seconds or {:.2f} minutes or {:.2f} hours or {:.2f} days'.format(crack_time_seconds, crack_time_minutes, crack_time_hours, crack_time_days))
+
+# calculate time to brute force password with given guess rate
+# lowercase 6 letter: password = 26^6 = 308915776 search space
+# guess rate of 15000/second
+# 308,915,776 / 15000 = 20594 seconds to search entire space
