63b4b9f97fe5f0bba6f399094e5cfee4dc5f2256
tappyy <apottage@gmail.com>
2017-12-30 00:51:20 +0000

    added csv export

--- benchmark.py
+++ benchmark.py
@@ -9,9 +9,13 @@ import argparse
 import random
 import wmi
 
+# todo
+# check if computer is windows or linux
+# get MAX_GUESS_LIMIT from config file [x]
+# format outputs
+
 CONFIG_FILE = 'config.ini'
 RANDOM_PASSWORD_LENGTH = 10
-MAX_GUESS_LIMIT = 5000000
 computer = wmi.WMI()
 cpu_info = computer.Win32_Processor()[0]
 
@@ -25,6 +29,13 @@ def get_guess_rate():
     guess_rate = config.get('cpu_settings', 'guess_rate')
     return guess_rate
 
+def get_benchmark_guesses():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+
+    guess_limit = config.get('general_settings', 'benchmark_guesses')
+    return guess_limit
+
 def timed_brute_force():
     # add progress bar
     time_start = time.time()
@@ -32,17 +43,16 @@ def timed_brute_force():
     random_password = create_random_password()
     print('CPU: {}'.format(str(cpu_info.Name)))
     print('Logical Cores: {}'.format(os.cpu_count()))
-    print('Random password: {}'.format(random_password))
+    benchmark_guesses = int(get_benchmark_guesses())
 
     for guess in itertools.product(string.ascii_letters + string.punctuation, repeat=RANDOM_PASSWORD_LENGTH):
         guesses += 1
-        # print('>>> Attempts: {}'.format(guesses), end='\r', flush=True)
-        if(guesses == MAX_GUESS_LIMIT):
+        if(guesses == benchmark_guesses):
             time_finish = time.time()
             time_taken = time_finish - time_start
             guess_rate = guesses / time_taken
             update_config_file('{:.0f}'.format(guess_rate))
-            print('Guess target of {} reached. Guess rate per second: {:.0f} - Time taken: {:.2f} seconds'.format(MAX_GUESS_LIMIT, guess_rate, time_taken))
+            print('Guess target of {} reached. Guess rate per second: {:.0f} - Time taken: {:.2f} seconds'.format(benchmark_guesses, guess_rate, time_taken))
             break
 
 def create_random_password():
@@ -57,7 +67,10 @@ def create_config_file():
     config.add_section('cpu_settings')
     config.set('cpu_settings', 'cpu_model', str(cpu_info.Name))
     config.set('cpu_settings', 'cpu_logical', str(os.cpu_count()))
-    config.set('cpu_settings', 'guess_rate', str(15000)) # default value
+    config.set('cpu_settings', 'guess_rate', str(0)) # default value
+
+    config.add_section('general_settings')
+    config.set('general_settings', 'benchmark_guesses', str(10000000)) # default value - 10 mil
 
     with open(CONFIG_FILE, "w") as f:
         config.write(f)
--- qwertyuiop.py
+++ qwertyuiop.py
@@ -2,36 +2,48 @@
 from benchmark import *
 import os.path
 import argparse
+import pandas as pd
 
-# add output to csv option
+# todo
+# detect search space size (find lowercase, uppercase, digits and punctuation in the string)
+# add option for input txt file instead of pwd args
+
 parser = argparse.ArgumentParser()
-parser.add_argument("pwd", type=str,
+parser.add_argument("pwd", type=str, nargs='+', default=[],
                     help="The password to test")
 parser.add_argument("-b", "--benchmark",
                     help="Perform a benchmark to get guess rate", action="store_true")
+parser.add_argument("-o", "--output",
+                    help="Filename to output to csv")
 
 args = parser.parse_args()
-password = args.pwd
 
 # create config file if it doesn't exist
 if not(os.path.isfile(CONFIG_FILE)):
     create_config_file()
 else:
-    if(args.benchmark): # stops performing benchmark twice
+    if(args.benchmark):
         timed_brute_force()
 
 guess_rate = int(get_guess_rate())
-search_space = 26**len(password)
 print('guess rate is {} guesses per second'.format(guess_rate))
 
-crack_time_seconds = search_space / guess_rate
-crack_time_minutes = crack_time_seconds / 60
-crack_time_hours = crack_time_minutes / 60
-crack_time_days = crack_time_hours / 24
+# init lists for csv output
+if(args.output):
+    passwords = []
+    crack_times = []
+    search_spaces = []
 
-print('Estimated time to crack password: {:.2f} seconds or {:.2f} minutes or {:.2f} hours or {:.2f} days'.format(crack_time_seconds, crack_time_minutes, crack_time_hours, crack_time_days))
+for password in args.pwd:
+    search_space = 26**len(password)
+    crack_time_seconds = search_space / guess_rate
 
-# calculate time to brute force password with given guess rate
-# lowercase 6 letter: password = 26^6 = 308915776 search space
-# guess rate of 15000/second
-# 308,915,776 / 15000 = 20594 seconds to search entire space
+    if(args.output):
+        passwords.append(password)
+        crack_times.append('{:.3f}'.format(crack_time_seconds))
+        search_spaces.append(search_space)
+
+if(args.output):
+    df_data = {'Password':passwords, 'Crack Time': crack_times, 'Search Space':search_spaces}
+    df = pd.DataFrame(df_data)
+    df.to_csv(args.output,index=False)
